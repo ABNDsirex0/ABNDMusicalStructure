7288c0fb5a3fc93d45c7986b07a72fdcac030420
Krzysztof Kin <sirex0@gmail.com>
2018-03-09 13:15:27 +0100

    fonts to style

--- app/src/main/java/cloud/krzysztofkin/musicalstructure/PlayerActivity.java
+++ app/src/main/java/cloud/krzysztofkin/musicalstructure/PlayerActivity.java
@@ -4,21 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity {
     private ArrayList<Track> tracksOnList;
     private ArrayList<Track> tracksToPlay;
+    int currentTrack;
+    TextView albumTitle;
+    TextView trackTitle;
+    TextView counter;
+    ProgressBar progressBar;
+    ImageView albumCover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Track track;
+        currentTrack = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         Intent i = getIntent();
         tracksOnList = i.getParcelableArrayListExtra("tracksOnList");
-        //todo przesylanie 1 utworu do grania i całej listy by można było powrócić (hasextra trackto play)
         if (i.hasExtra("trackToPlay")) {
             track = i.getParcelableExtra("trackToPlay");
             tracksToPlay = new ArrayList<Track>();
@@ -27,6 +37,13 @@ public class PlayerActivity extends AppCompatActivity {
             tracksToPlay = tracksOnList;
         }
 
+        albumTitle = findViewById(R.id.album_title);
+        trackTitle = findViewById(R.id.track_title);
+        counter = findViewById(R.id.counter);
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar.setMax(tracksToPlay.size());
+        albumCover = findViewById(R.id.album_cover);
+        refreschPlayer();
     }
 
     public void goToMain(View view) {
@@ -38,5 +55,34 @@ public class PlayerActivity extends AppCompatActivity {
         Intent i = new Intent(this, TracksActivity.class);
         i.putParcelableArrayListExtra("tracksOnList", tracksOnList);
         startActivity(i);
+    }
+
+    public void onPrevious(View view) {
+        if (currentTrack > 0) {
+            --currentTrack;
+        }
+        refreschPlayer();
+    }
+
+    public void onPlay(View view) {
+        String textToToast;
+        textToToast = "Alphabet/" + tracksToPlay.get(currentTrack).getAlbum() + "/" + tracksToPlay.get(currentTrack).getTitle();
+        Toast.makeText(this, textToToast, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNext(View view) {
+        if (currentTrack < tracksToPlay.size() - 1) {
+            ++currentTrack;
+        }
+        refreschPlayer();
+    }
+
+    private void refreschPlayer() {
+        int coverId = getResources().getIdentifier(tracksToPlay.get(currentTrack).getCover(), "drawable", getPackageName());
+        albumCover.setImageResource(coverId);
+        albumTitle.setText(tracksToPlay.get(currentTrack).getAlbum());
+        trackTitle.setText((tracksToPlay.get(currentTrack).getTitle()));
+        counter.setText(currentTrack + 1 + "/" + tracksToPlay.size());
+        progressBar.setProgress(currentTrack + 1);
     }
 }
